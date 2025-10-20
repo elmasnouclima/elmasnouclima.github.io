9d3db5bb6365aa2a16acf01e8aea5f9b80cab80b
elmasnouclima <fmp14352@gmail.com>
2025-10-20 20:08:33 +0200

    Update wu.js

--- assets/wu.js
+++ assets/wu.js
@@ -1,3 +1,5 @@
+// TEST: debe cambiar el texto inmediatamente al cargar el script
+document.getElementById('wu-status').textContent = 'Script WU arrancó';
 // antes:
 const CACHE_NAME = 'estacion-pwa-v1';
 // ahora:
